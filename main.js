7c093cbe9b6a84ab3c51268e447ee778170c622c
sergdz <sergeydzubenko1@gmail.com>
2022-03-24 09:40:46 +0200

    final commit

--- main.js
+++ main.js
@@ -1,3 +1,62 @@
-let oper01 = prompt("Введите ваше число!")
-let oper02 = prompt()
-prompt()
+
+
+let a  = Number (prompt("Введите первое число!" ));
+
+let b =  prompt("Введите действие");
+
+let c = Number (prompt( "Введите второе число"));
+            
+
+let result;
+
+
+
+
+
+
+if (b === "+") {
+    result = a + c;
+    alert(result)
+    
+}
+
+
+
+
+
+if (b === "/") {
+    result = a / c;
+    alert (result);
+    
+    
+}
+
+
+
+
+if (b === "-") {
+    result = a - c;
+    alert (result);
+    
+}
+
+
+
+if ( b === "*") {
+    result = a * c;
+    alert (result);
+    
+}
+
+if ( b !== "+" && b !== "-" && b !== "/" && b !== "*" ) {
+    result = alert("Не верный символ")
+}
+
+
+
+
+
+
+
+
+    
